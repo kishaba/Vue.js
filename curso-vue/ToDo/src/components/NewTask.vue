b8564e26ae3249153d1d3ab7a84be18d65085c73
Alexandre <alexandre.azevedo.mrus@gmail.com>
2020-02-03 17:57:35 -0300

    arrumando os bagulete

---
+++ curso-vue/ToDo/src/components/NewTask.vue
@@ -0,0 +1,63 @@
+<template>
+  <div class="new-task">
+    <input
+      v-model='name'
+      @keydown.enter='add'
+      type="text"
+      class='form-element'
+      placeholder="Nova tarefa?"
+    >
+    <button
+      class='botao'
+      @click="add"
+    >+</button>
+  </div>
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+      name: ''
+    }
+  },
+  methods: {
+    add () {
+      this.$emit('taskAdded', { name: this.name })
+      this.name = ''
+
+    }
+  }
+
+}
+</script>
+
+<style>
+.new-task {
+  margin: 35px;
+}
+.form-element {
+  outline: none;
+  font-size: 2rem;
+  padding: 5px 10px 8px;
+  color: #fff;
+}
+input.form-element {
+  border-right: none;
+  width: 500px;
+  background: #fff2;
+  border-top-left-radius: 8px;
+  border-bottom-left-radius: 8px;
+}
+
+.botao{
+  outline: none;
+  font-size: 2rem;
+  padding: 5px 10px 8px;
+  color: #fff;
+  border-left: none;
+  background-color: #2196f3;
+  border-top-right-radius: 8px;
+  border-bottom-right-radius: 8px;
+}
+</style>
---
+++ curso-vue/ToDo/src/components/TaskGrid.vue
@@ -0,0 +1,45 @@
+<template>
+  <div class="task-grid">
+    <template v-if='tasks.length'>
+      <Task
+        v-for='(task,i) in tasks'
+        :key='task.name'
+        @taskStateChanged='$emit("taskStateChanged",i)'
+        @taskDeleted='$emit("taskDeleted",i)'
+        :task='task'
+      >
+      </Task>
+    </template>
+    <p v-else class="no-task">Sua vida está em dia :)</p>
+  </div>
+
+</template>
+
+<script>
+import Task from '@/components/Task.vue'
+export default {
+  components: { Task },
+  props: {
+    tasks: { type: Array, required: true }
+  }
+}
+</script>
+
+<style>
+.task-grid{
+  display:flex;
+  justify-content: center;
+  flex-wrap: wrap; /*permite quebrar linhas*/
+  align-items: center;
+
+}
+
+.task-grid .task{
+  margin: 10px;
+}
+
+.no-task{
+  color: #aaaaaa;
+  font-size: 1.7rem;
+}
+</style>
